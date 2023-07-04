0954d55643e73b99c96034b0e4db4f4533998c3f
faith kaburu <faith.kaburu@student.moringaschool.com>
2023-07-05 00:16:03 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+//function writeCards(names,event){
+    //const thankYouCards=[];
+    //for(let i=0;i<names.length;i++){
+   // return thankYouCards;
+   // const message=['Thank you, ${Charlie},for the wonderful ${event}gift!']
+ //'Thank you, ${Samip},for the wonderful ${event}gift!'
+ // 'Thank you, ${Ali},for the wonderful ${event}gift!';
+//}
+  //  return thankYouCards;
+//}
+//const names=["Charlie","Samip","Ali"];
+//const event="birthday";
+//const thankYouCards=writeCards(names,events);
+//console.log(thankYouCards)
+
+function countDown(number)
+
+{while (number>=0){
+    console.log(number);
+    number--;}
+}
+countDown(10);
